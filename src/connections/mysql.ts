e5c2fafdc9127ea58acefaf4ffe335bad3999e66
llOrtegall <IvanOrtega_97@hotmail.com>
2024-04-30 09:18:13 -0500

    organizando y respaldado db

--- src/connections/mysql.ts
+++ src/connections/mysql.ts
@@ -3,7 +3,6 @@ import 'dotenv/config'
 
 export const pool_mysql = createPool({
   host: process.env.MYSQL_HOST,
-  port: parseInt(process.env.MYSQL_PORT as string),
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE,
