3047de93d357fdedeae0e25875be24404002e90d
llOrtegall <IvanOrtega_97@hotmail.com>
2024-12-24 11:03:50 -0500

    add MySQL connection test and import environment variables in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import { UserRoutes } from './routes/users.routes'
+import { pool_mysql } from './connections/mysql'
+import { PORT } from './config/enviroments'
 import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
-import 'dotenv/config'
 
 const app = express();
-const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
@@ -16,3 +16,15 @@ app.use('/api', UserRoutes)
 app.listen(PORT, () => {
   console.log(`Server Is Running On  http://localhost:${PORT}`);
 });
+
+// Test Connection
+pool_mysql.pool.getConnection((err, connection) => {
+  if (err) throw err;
+
+  connection.query('SELECT 1 + 1 ', (err, result) => {
+    connection.release();
+    if (err) throw err;
+    console.log('Database Connected', result);
+  });
+
+})
